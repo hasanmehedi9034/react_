88cdf1ac789a14753906bf8f0734ce81b55747dd
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-07 22:55:39 +0600

    increment using react

--- increment.js
+++ increment.js
@@ -1,6 +1,18 @@
 const domContainer = document.querySelector('#root');
 
-const myElement = React.createElement('div', null, 'Hello World');
+// const myElement = React.createElement(
+//     'div', 
+//     null,
+//     React.createElement('p', null, 'Hello Mehedi'));
+
+const myElement = React.createElement(
+    'div', 
+    null,
+    [
+        React.createElement('p', null, 'Hello Mehedi 1'),
+        React.createElement('p', null, 'Hello Mehedi 2')
+    ]
+)
 
 ReactDOM.render(myElement, domContainer);
 
