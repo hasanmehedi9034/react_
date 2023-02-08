17cf7c0b16faaf488cc816932d5515e6b923d5fe
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-08 11:56:55 +0600

    react state

--- increment.js
+++ increment.js
@@ -5,16 +5,44 @@ const domContainer = document.querySelector('#root');
 //     null,
 //     React.createElement('p', null, 'Hello Mehedi'));
 
-const myElement = React.createElement(
-    'div', 
-    null,
-    [
-        React.createElement('p', null, 'Hello Mehedi 1'),
-        React.createElement('p', null, 'Hello Mehedi 2')
-    ]
-)
+// const myElement = React.createElement(
+//     'div', 
+//     null,
+//     [
+//         React.createElement('p', null, 'Hello Mehedi 1'),
+//         React.createElement('p', null, 'Hello Mehedi 2')
+//     ]
+// )
 
-ReactDOM.render(myElement, domContainer);
+const Increment = () => {
+    const [counter, setCounter] = React.useState(0);
+
+    return (
+        <div>
+            <h1 id="display">{ counter }</h1>
+            <div>
+                <button id="button" onClick={() => setCounter(counter + 1)}>Increment</button>
+            </div>
+        </div>
+    )
+}
+
+
+// ReactDOM.render(Increment(), domContainer);
+ReactDOM.render( <Increment />, domContainer);
+
+
+
+// const display = document.querySelector('#display');
+// const button = document.querySelector('#button');
+
+// let count = parseInt(display.textContent);
+
+// button.addEventListener('click', () => {
+//     count++;
+//     display.textContent = count;
+// })
+
 
 
 
