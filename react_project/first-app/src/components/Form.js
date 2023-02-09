4d46d01d2f3318ac1187d88e48f367394ca1dd43
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-09 14:24:33 +0600

    form handling

--- react_project/first-app/src/components/Form.js
+++ react_project/first-app/src/components/Form.js
@@ -2,20 +2,79 @@ import React from "react";
 
 export default class Form extends React.Component {
     state = {
-        title: 'JavaScript'
+        title: 'JavaScript',
+        text: 'JS is awesome',
+        library: 'react',
+        isAwesome: false
     }
 
-    handleChange = (e) => {        
-        this.setState({
-            title: e.target.value
-        });
+    handleChange = (e) => {  
+        if (e.target.type === 'text') {
+            this.setState({
+                title: e.target.value
+            });
+        } 
+        else if(e.target.type === 'textarea') {
+            this.setState({
+                text: e.target.value
+            });
+        }     
+        else if(e.target.type === 'select-one') {
+            this.setState({
+                library: e.target.value
+            });
+        }     
+        else if(e.target.type === 'checkbox') {
+            this.setState({
+                isAwesome: e.target.checked
+            })
+        }
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault();
+        console.log(this.state)
     }
 
     render() {
         return (
             <div>
-                <form>
-                    <input type="text" placeholder="Enter Title" value={this.state.title} onChange={this.handleChange} />
+                <form onSubmit={this.submitHandler}>
+                    <input 
+                        type="text"
+                        placeholder="Enter Title"
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <br />
+
+                    <textarea name='text'
+                        value={this.state.text}
+                        onChange={this.handleChange}
+                    >
+                    </textarea>
+                    <br />
+                    <br />
+
+                    <select
+                        value={this.state.library}
+                        onChange={this.handleChange}
+                    >
+                        <option value='react'>React</option>
+                        <option value='angular'>Angular</option>
+                    </select>
+                    <br />
+                    <br />
+
+                    <input type="checkbox"
+                        checked={this.state.isAwesome}
+                        onChange={this.handleChange}
+                    />
+                    <br/>
+                    <br/>
+
+                    <input type='submit' value="submit" />
                 </form>
             </div>
         )
