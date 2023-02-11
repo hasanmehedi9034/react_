d6ab3295efaa7111d7702f1a5b11913893b5e38e
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-11 15:11:35 +0600

    useCallback for optimize re-rendering

--- react_project/react-hook/src/App.js
+++ react_project/react-hook/src/App.js
@@ -2,15 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 import MyComponentClass from './components/MyComponentClass';
 import MyComponent from './components/MyComponent';
+import { useCallback, useState } from 'react';
+import ShowCount from './components/ShowCount';
+import Button from './components/Button';
+import Title from './components/Title';
 
 function App() {
-  return (
-      <>
-        <p1>We shall learn useEffect() today</p1>
-        {/* <MyComponentClass/> */}
-        <MyComponent/>
-      </>
-  );
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    const incrementByOne = useCallback(() => {
+        setCount1((count1) => count1 + 1);
+    }, []);
+
+    const incrementByFive = useCallback(() => {
+        setCount2((count2) => count2 + 5);
+    }, []);
+
+    return (
+        <>  
+            <Title/>
+
+            <ShowCount count={count1} title='Counter 1'/>
+
+            <Button handleClick={incrementByOne}>
+                Increment by one
+            </Button>
+
+            <hr/>
+
+            <ShowCount count={count2} title='Counter 2'/>
+
+            <Button handleClick={incrementByFive}>
+                Increment by five
+            </Button>
+
+        </>
+    );
 }
 
 export default App;
