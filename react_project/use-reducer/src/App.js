6a642a99b361b7eaa7e446d9e31f7500879b322e
hasanmehedi9034 <mehedi90344@gmail.com>
2023-02-12 11:12:51 +0600

    userReducer

---
+++ react_project/use-reducer/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+import ComplexCounter from './components/ComplexCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <ComplexCounter/>
+    </div>
+  );
+}
+
+export default App;
